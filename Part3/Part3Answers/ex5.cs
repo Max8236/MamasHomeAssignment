902057962386f998f25a3da0bc9b9f40f57f4832
Max Levin <maxlev823@gmail.com>
2024-05-07 00:19:40 +0300

    added initial code asnwers for part 3(ex5,6)

---
+++ Part3/Part3Answers/ex5.cs
@@ -0,0 +1,32 @@
+﻿public class Node
+{
+    private int _value;
+    private Node _next;
+    public Node(int value)
+    {
+        this._value = value;
+        this._next = null;
+    }
+    public Node(int value, Node next)
+    {
+        this._value = value; 
+        this._next = next;
+    }
+    public void setValue(int value)
+    {
+        this._value = value;
+    }
+    public int getValue()
+    {
+        return this._value;
+    }
+    public void setNext(Node next)
+    {
+            this._next = next;
+    }
+    public Node getNext()
+    {
+        return this._next;
+    }
+
+}
---
+++ Part3/Part3Answers/ex6.cs
@@ -0,0 +1,103 @@
+﻿public class LinkedList
+{
+    private Node _head;
+    
+    public void append(int num)
+    {
+        Node tempHead = _head;
+        if (tempHead != null )
+        {
+            while (tempHead.getNext() != null)
+            {
+                tempHead = tempHead.getNext();
+            }
+            tempHead.setNext(new Node(num));
+        }
+        else
+        {
+            this._head = new Node(num);
+        }
+    }
+    public void prepend(int num)
+    {
+        Node newHead = new Node(num,this._head);
+        this._head = newHead;
+    }
+    public int pop()
+    {
+        if (this._head != null )
+        {
+            Node tempHead = _head;
+            Node lastNode = tempHead;
+
+            while (tempHead.getNext() != null)
+            {
+                lastNode = tempHead;
+                tempHead = tempHead.getNext();
+            }
+            lastNode.setNext(null);
+            return tempHead.getValue();
+        }
+        return 0;
+       
+    }
+    public int unqueue()
+    {
+        if(this._head != null )
+        {
+            Node tempHead = _head;
+            this._head = this._head.getNext();
+            return tempHead.getValue();
+        }
+        return 0;
+       
+    }
+    public IEnumerable<int> toList() 
+    {
+        IEnumerable<int> list = new List<int>();
+        Node tempHead = this._head;
+        while (tempHead != null)
+        {
+            list.Append(tempHead.getValue());
+            tempHead = tempHead.getNext();
+        }
+        return list;
+    }
+    public bool isCircular()
+    {
+        if (this._head != null)
+        {
+            Node tempHead = this._head;
+            while(tempHead != null)
+            {
+                tempHead = tempHead.getNext();
+                if (tempHead == this._head)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public void sort()
+    {
+        // to do
+    }
+    public Node getMaxNode()
+    {
+
+        Node tempHead = this._head;
+        Node max = tempHead;
+        while (tempHead != null)
+        {
+            if(tempHead.getValue() > max.getValue())
+            {
+                max = tempHead;
+            }
+            tempHead = tempHead.getNext();
+        }
+        return max;
+
+
+    }
+}
